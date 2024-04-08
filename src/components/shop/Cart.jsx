f64c307a675f8b38ad44189db7916c3e81cf4cdb
Priyanshu Semwal <pxyz943@gmail.com>
2024-04-09 02:03:35 +0530

    fixed cart links

--- src/components/shop/Cart.jsx
+++ src/components/shop/Cart.jsx
@@ -24,7 +24,7 @@ export default function Cart(){
 
             let data = await response.json();
             setCart(data.cart.map((item,index)=>{
-                return <Link  key={index} className='link-component' to={`http://localhost:5173/product?id=${item.product}`}><CartItem product={item} updateCart={getCart}/></Link>
+                return <Link  key={index} className='link-component' to={`http://thefunkyverse.com/product?id=${item.product}`}><CartItem product={item} updateCart={getCart}/></Link>
                 
             }));
         }
